1e61379930bc24351aca4a91af20bde21f66b61b
Lilia Stoykova <lilia.stoykov@gmail.com>
2026-03-07 16:30:27 +0200

    updated movie_router

--- routers/movie_router.py
+++ routers/movie_router.py
@@ -4,7 +4,7 @@ from data.models import MovieCreate
 
 movie_router = APIRouter(prefix="/movies")
 
-@movie_router.post("/")
+@movie_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_movies(data: MovieCreate):
     movie_id = create_new_movie(data)
     return {"id": movie_id, "message": "Movie created."}
@@ -17,6 +17,13 @@ def get_all_movies(search: str | None = None, sort: str | None = None):
 def get_movies_by_id(movie_id: int):
     return get_by_id(movie_id)
 
+@movie_router.put("/{movie_id}")
+def update_movie(movie_id: int, data: MovieUpdate):
+    return change_movie(movie_id, data)
+
+@movie_router.delete("/{movie_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_movie(movie_id: int):
+    return remove_movie(movie_id)
 
 
 
