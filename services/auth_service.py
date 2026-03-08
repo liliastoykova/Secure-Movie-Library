b162d6606d8b46e065ce923234f9da5675f51864
Lilia Stoykova <lilia.stoykov@gmail.com>
2026-03-08 14:25:17 +0200

    made it as a class

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,11 +1,14 @@
 from auth.hashing import verify_password
-from repositories.users_repository import get_user_by_username
+from repositories.users_repository import UserRepository
 
+class AuthService:
+    def __init__(self, user_repo: UserRepository):
+        self.user_repo = user_repo
 
-def authenticate_user(username: str, password: str):
-    user = get_user_by_username(username)
-    if not user:
-        return False
-    if not verify_password(password, user.password):
-        return False
-    return user
+    def authenticate_user(self, username: str, password: str):
+        user = self.user_repo.get_user_by_username(username)
+        if not user:
+            return False
+        if not verify_password(password, user.password):
+            return False
+        return user
