5892fca2b06fc8084b21fc06c787b44468e79ce7
Lilia Stoykova <lilia.stoykov@gmail.com>
2026-03-08 14:13:27 +0200

    updated files to be as class

--- repositories/movies_repository.py
+++ repositories/movies_repository.py
@@ -1,101 +1,102 @@
 from data.database import read_query, insert_query, update_query
 from data.models import Movie
 
+class MovieRepository:
 
-def create_movie(title: str, director: str, release_year: int):
-    sql = """INSERT INTO movies (title, director, release_year)
-            VALUES (?, ?, ?)"""
+    def create_movie(self, title: str, director: str, release_year: int):
+        sql = """INSERT INTO movies (title, director, release_year)
+                VALUES (?, ?, ?)"""
 
-    return insert_query(sql, (title, director, release_year))
+        return insert_query(sql, (title, director, release_year))
 
-def get_movies(search: str | None, sort: str | None) -> list[Movie]:
-    sort_sql = """SELECT id, title, director, release_year, rating 
-                    FROM movies
-                    ORDER BY rating DESC"""
+    def get_movies(self, search: str | None, sort: str | None) -> list[Movie]:
+        sort_sql = """SELECT id, title, director, release_year, rating 
+                        FROM movies
+                        ORDER BY rating DESC"""
 
-    search_sql = """SELECT id, title, director, release_year, rating
-                    FROM movies
-                    WHERE title LIKE ?"""
+        search_sql = """SELECT id, title, director, release_year, rating
+                        FROM movies
+                        WHERE title LIKE ?"""
 
-    sql = """SELECT id, title, director, release_year, rating
-                FROM movies"""
+        sql = """SELECT id, title, director, release_year, rating
+                    FROM movies"""
 
-    if search:
-        rows =  read_query(search_sql, (f"%{search}%",))
-    elif sort == 'rating':
-        rows = read_query(sort_sql)
-    else:
-        rows = read_query(sql)
+        if search:
+            rows =  read_query(search_sql, (f"%{search}%",))
+        elif sort == 'rating':
+            rows = read_query(sort_sql)
+        else:
+            rows = read_query(sql)
 
-    movies = []
+        movies = []
 
-    for row in rows:
-        movie = Movie(id=row[0],
-              title=row[1],
-              director=row[2],
-              release_year=row[3],
-              rating=row[4]
-        )
-        movies.append(movie)
-
-    return movies
-
-
-def get_movie_by_id(id: int):
-    sql = """SELECT id, title, director, release_year, rating
-                FROM movies
-                WHERE id = ?"""
-
-    rows = read_query(sql, (id, ))
-
-    if not rows:
-        return None
-
-    row = rows[0]
-
-    movie = Movie(id=row[0],
-                title=row[1],
-                director=row[2],
-                release_year=row[3],
-                rating=row[4]
+        for row in rows:
+            movie = Movie(id=row[0],
+                  title=row[1],
+                  director=row[2],
+                  release_year=row[3],
+                  rating=row[4]
             )
+            movies.append(movie)
 
-    return movie
+        return movies
 
-def get_movie_by_title(title: str):
-    sql = """SELECT id
-                FROM movies
-                WHERE title = ?"""
 
-    rows = read_query(sql, (title, ))
+    def get_movie_by_id(self, id: int):
+        sql = """SELECT id, title, director, release_year, rating
+                    FROM movies
+                    WHERE id = ?"""
 
-    if not rows:
-        return None
+        rows = read_query(sql, (id, ))
 
-    return rows[0]
+        if not rows:
+            return None
 
-def get_movie_by_title_and_year(title: str, release_year: int):
-    sql = """SELECT id
-                FROM movies
-                WHERE title = ? AND release_year = ?"""
+        row = rows[0]
 
-    rows = read_query(sql, (title, release_year))
+        movie = Movie(id=row[0],
+                    title=row[1],
+                    director=row[2],
+                    release_year=row[3],
+                    rating=row[4]
+                )
 
-    if not rows:
-        return None
+        return movie
 
-    return rows[0][0]
+    def get_movie_by_title(self, title: str):
+        sql = """SELECT id
+                    FROM movies
+                    WHERE title = ?"""
 
-def update_movie(movie_id: int, title: str | None, director: str | None, release_year: int | None):
-    sql = """UPDATE movies
-                SET title = ?, director = ?, release_year = ?
-                WHERE id = ?"""
+        rows = read_query(sql, (title, ))
 
-    update_query(sql, (title, director, release_year, movie_id))
+        if not rows:
+            return None
 
-def delete_movie(movie_id: int):
-    sql = """DELETE FROM movies WHERE id = ?"""
+        return rows[0][0]
 
-    update_query(sql, (movie_id, ))
+    def get_movie_by_title_and_year(self, title: str, release_year: int):
+        sql = """SELECT id
+                    FROM movies
+                    WHERE title = ? AND release_year = ?"""
+
+        rows = read_query(sql, (title, release_year))
+
+        if not rows:
+            return None
+
+        return rows[0][0]
+
+    def update_movie(self, movie_id: int, title: str | None, director: str | None, release_year: int | None):
+        sql = """UPDATE movies
+                    SET title = ?, director = ?, release_year = ?
+                    WHERE id = ?"""
+
+        update_query(sql, (title, director, release_year, movie_id))
+
+    def delete_movie(self, movie_id: int):
+        sql = """DELETE FROM movies WHERE id = ?"""
+
+        update_query(sql, (movie_id, ))
 
 
--- services/movie_service.py
+++ services/movie_service.py
@@ -2,50 +2,53 @@ from fastapi import HTTPException
 from data.models import MovieCreate, MovieUpdate
 from repositories.movies_repository import *
 
+class MovieService:
+    def __init__(self, movie_repo: MovieRepository):
+        self.movie_repo = movie_repo
 
-def create_new_movie(data: MovieCreate):
-    movie_exists = get_movie_by_title_and_year(data.title, data.release_year)
+    def create_new_movie(self, data: MovieCreate):
+        movie_exists = self.movie_repo.get_movie_by_title_and_year(data.title, data.release_year)
 
-    if movie_exists:
-        raise HTTPException(409, "Movie already exists")
+        if movie_exists:
+            raise HTTPException(409, "Movie already exists")
 
-    movie_id = create_movie(data.title, data.director, data.release_year)
-    return movie_id
+        movie_id = self.movie_repo.create_movie(data.title, data.director, data.release_year)
+        return movie_id
 
-def get_all(search: str | None, sort: str | None):
-    return get_movies(search, sort)
+    def get_all(self, search: str | None, sort: str | None):
+        return self.movie_repo.get_movies(search, sort)
 
-def get_by_id(movie_id: int):
-    movie = get_movie_by_id(movie_id)
+    def get_by_id(self, movie_id: int):
+        movie = self.movie_repo.get_movie_by_id(movie_id)
 
-    if movie is None:
-        raise HTTPException(status_code=404, detail="Movie not found.")
+        if movie is None:
+            raise HTTPException(status_code=404, detail="Movie not found.")
 
-    return movie
+        return movie
 
-def change_movie(movie_id: int, data: MovieUpdate):
-    movie = get_movie_by_id(movie_id)
+    def change_movie(self, movie_id: int, data: MovieUpdate):
+        movie = self.movie_repo.get_movie_by_id(movie_id)
 
-    if movie is None:
-        raise HTTPException(status_code=404, detail="Movie not found.")
+        if movie is None:
+            raise HTTPException(status_code=404, detail="Movie not found.")
 
-    title = data.title if data.title is not None else movie.title
-    director = data.director if data.director is not None else movie.director
-    release_year = data.release_year if data.release_year is not None else movie.release_year
+        title = data.title if data.title is not None else movie.title
+        director = data.director if data.director is not None else movie.director
+        release_year = data.release_year if data.release_year is not None else movie.release_year
 
-    existing = get_movie_by_title_and_year(title, release_year)
+        existing = self.movie_repo.get_movie_by_title_and_year(title, release_year)
 
-    if existing and existing != movie_id:
-        raise HTTPException(409, "Movie with this title and year already exists.")
+        if existing and existing != movie_id:
+            raise HTTPException(409, "Movie with this title and year already exists.")
 
-    update_movie(movie_id, title, director, release_year)
+        self.movie_repo.update_movie(movie_id, title, director, release_year)
 
-    return {"message": "Movie updated."}
+        return {"message": "Movie updated."}
 
-def remove_movie(movie_id: int):
-    movie = get_movie_by_id(movie_id)
+    def remove_movie(self, movie_id: int):
+        movie = self.movie_repo.get_movie_by_id(movie_id)
 
-    if movie is None:
-        raise HTTPException(status_code=404, detail="Movie not found.")
+        if movie is None:
+            raise HTTPException(status_code=404, detail="Movie not found.")
 
-    delete_movie(movie.id)
+        self.movie_repo.delete_movie(movie.id)
